a8fd6162c13805792edd59d91b25d2ba85f4385a
Egor Ponomarev <egor@ponomarev.pp.ru>
2017-09-13 23:52:01 +0300

    first homework

--- homework_1.js
+++ homework_1.js
@@ -1,25 +1,73 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+app.listen(1337);
 
 const restV1 = express.Router();
 
-restV1.get("/users/", (req, res) => {
+const users = [{
+    name: 'Egor',
+    score: '1290'
+  },
+  {
+    name: 'Stepan',
+    score: '1570'
+  }
+];
 
+restV1.get('/users/', (req, res) => {
+  res.json(users);
 });
 
-restV1.post("/users/", (req, res) => {
-
+restV1.post('/users/:name/score/:score', (req, res) => {
 
   const user = {
-    'name': req.param.name,
-    'score': req.param.name,
-    toString() {
-      return `name: ${this.name}
-            score: ${this.score}`
-    }
+    name: req.params.name,
+    score: req.params.score
+  };
+  console.log(users.find(el => { return el.name === req.params.name }));
+
+  if (users.find(el => { return el.name === req.params.name }) === undefined) {
+    users.push(user);
+
+    res.json(users);
+  } else {
+    res.sendStatus(400);
   }
 
-  res.send(user);
 });
 
-app.use("/api/v1", restV1);
+restV1.delete('/users/:name/', (req, res) => {
+  const index = users.findIndex((el) => {
+    return el.name === req.params.name;
+  });
+
+  if (index >= 0) {
+    users.splice(index, 1);
+
+    res.json(users);
+  } else {
+    res.sendStatus(404);
+  }
+
+})
+
+restV1.put('/users/:name/score/:score', (req, res) => {
+  const element = users.find(el => {
+    if (el.name === req.params.name) {
+      el.score = req.params.score;
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  if (element === undefined) {
+    res.sendStatus(404);
+  } else {
+    res.json(users);
+  }
+
+});
+
+
+app.use('/api/v1', restV1);
